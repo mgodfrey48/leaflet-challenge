7d214950c48d0005c57175d8977f2d41aa180240
Matthew Godfrey <matthewgodfrey@MacBook-Pro.local>
2022-01-05 17:27:18 -0600

    Got the circle markers functioning properly. Added a fucntion to determine the color of the markers.

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,37 +1,29 @@
-// Create a map
-function createMap(earthquakes) {
-
-    let lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "light-v10",
-    accessToken: API_KEY
-    });
-
-    let baseMap = {
-        "Light Map": lightMap
-    };
-
-    let markerLayer = {
-        "Earthquakes": earthquakes
-    };
-
-    let map = L.map("map", {
-        center: [42.81, -107.09],
-        zoom: 5,
-        layers: [lightMap, earthquakes]
-    });
-}
-
 // Perform an API call to retrieve earthquake data
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
 
-// Add markers to the map
+// Create array of markers to use when creating the map
 function createMarkers(response) {
     console.log("create markers function in running");
     
     // Initialize an array to hold the earthquake markers
     quakeMarkers = [];
+
+    // Function to set the marker color
+    function markerColor(depth) {
+        if (depth >= -10 && depth < 10) {
+            return "#bfff80";
+        } else if (depth >= 10 && depth < 30) {
+            return "#ffff80";
+        } else if (depth >= 30 && depth < 50) {
+            return "#ffcc99";
+        } else if (depth >= 50 && depth < 70) {
+            return "#ff9933";
+        } else if (depth >= 70 && depth < 90) {
+            return "#e65c00";
+        } else if (depth >= 90) {
+            return "#cc2900";
+        } 
+    };
 
     // Loop through each earthquake and store the latitudes, longitudes, depths and magnitudes in markers
     let earthquakes = response.features;
@@ -42,16 +34,35 @@ function createMarkers(response) {
         let depth = coordinates[2];
         let size = earthquake.properties.mag;
 
-        quakeMarker = L.circle([lat, lon], {
-            color: "yellow",
-            fillColor: "yellow",
-            fillOpacity: depth,
-            radius: size
+        quakeMarker = L.circleMarker([lat, lon], {
+            color: "black",
+            weight: 1,
+            fillColor: markerColor(depth),
+            fillOpacity: .5,
+            radius: size * 4
         });
-
+        console.log(markerColor(depth))
         quakeMarkers.push(quakeMarker);
     });
 
+    // Create the map using markers array as a layer group
     createMap(L.layerGroup(quakeMarkers));
 }
 
+// Create the map
+function createMap(earthquakes) {
+    // Base layer
+    let lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
+    });
+
+    // Map with base layer and earthquake marker layer
+    let map = L.map("map", {
+        center: [42.81, -107.09],
+        zoom: 5,
+        layers: [lightMap, earthquakes]
+    });
+}
